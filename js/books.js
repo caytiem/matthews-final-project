5f6da9b4ab8ebfc6f05f5c08705a0804c284d844
caytiem <62902420+caytiem@users.noreply.github.com>
2020-06-08 17:28:41 -0700

    changes
    changes

---
+++ js/books.js
@@ -0,0 +1,32 @@
+const BOOKS = [
+    {
+        "title": "Emergent Strategy: Shaping Change, Changing Worlds",
+        "genre": "Nonfiction, Social Movements, Social Justice, Politics, Activism, Self Help, Feminism",
+        "author": "adrienne maree brown",
+        "pages": 274,
+    },
+    {
+        "title": "Sister Outsider",
+        "genre": "Nonfiction, Social Justice, Feminism, Race, Essays, Gender, Anti Racist",
+        "author": "Audre Lorde",
+        "pages": 190,
+    },
+    {
+        "title": "White Teeth",
+        "genre": "Fiction, Contemporary, Novels",
+        "author": "Zadie Smith",
+        "pages": 448,
+    },
+    {
+        "title": "Beloved",
+        "genre": "Fiction, Classics, Historical Fiction, Magical Realism",
+        "author": "Toni Morrison",
+        "pages": 324,
+    },
+    {
+        "title": "Feminism is for Everybody: Passion Politics",
+        "genre": "Nonfiction, Feminism, Gender, Politics",
+        "author": "bell hooks",
+        "pages": 123,
+    },
+];
